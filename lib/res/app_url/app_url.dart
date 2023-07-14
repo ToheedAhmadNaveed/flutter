95c13f8e7994ea2852702957cfee7c3a5377b357
ToheedAhmadNaveed <toheed.ahmad.4264@gmail.com>
2023-07-14 15:51:21 +0500

    install git

---
+++ lib/res/app_url/app_url.dart
@@ -0,0 +1,6 @@
+class AppUrl {
+  static const String base_url = "http://10.0.2.2:8000";
+  static const String login_url = "$base_url/api/user/login/";
+  static const String user_url =
+      "https://webhook.site/e3fc4b04-53d9-426f-a1c2-98ec81f5cac5";
+}
